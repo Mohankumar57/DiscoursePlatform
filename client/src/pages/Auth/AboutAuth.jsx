8336bcae362443a43683ad99f8df438160c3098d
Mohankumar M <mohankumar.cb21@bitsathy.ac.in>
2024-03-15 11:50:49 +0530

    feature signup working

---
+++ client/src/pages/Auth/AboutAuth.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Auth.css'
+
+const AboutAuth = () => {
+  return (
+    <div className="auth-container-1">
+      {/* <h1>Join the DiscoursePlatform community</h1>
+      */}
+    </div>
+  );
+};
+
+export default AboutAuth;
